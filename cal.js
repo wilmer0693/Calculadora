b63ea9daf406b9292177dd0627e18a1cbac5b38b
Wilmer <wgmartinez54@misena.edu.co>
2018-05-03 20:09:22 -0500

    mi primer calculadora

---
+++ cal.js
@@ -0,0 +1,43 @@
+
+function suma(){
+	var num1 =document.getElementById('num1').value;
+	var num2 =document.getElementById('num2').value;
+	var resul3=0;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resu=parseInt(resul3);
+	resul3=num1+num2;
+	resul3=document.getElementById('resul3').value=resul3;
+	return false;
+}
+function resta(){
+	var num1 =document.getElementById('num1').value;
+	var num2 =document.getElementById('num2').value;
+	var resul3=0;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resu=parseInt(resul3);
+	resul3=num1-num2;
+	resul3=document.getElementById('resul3').value=resul3;
+	return false;
+}function multiplicacion(){
+	var num1 =document.getElementById('num1').value;
+	var num2 =document.getElementById('num2').value;
+	var resul3=0;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resu=parseInt(resul3);
+	resul3=num1*num2;
+	resul3=document.getElementById('resul3').value=resul3;
+	return false;
+}function divicion(){
+	var num1 =document.getElementById('num1').value;
+	var num2 =document.getElementById('num2').value;
+	var resul3=0;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resu=parseInt(resul3);
+	resul3=num1%num2;
+	resul3=document.getElementById('resul3').value=resul3;
+	return false;
+}
